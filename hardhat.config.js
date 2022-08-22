3a310e7214b6a9b2c79f52a3a99304f9c1db6769
Ayomide Bajo <oluwashinabajo@gmail.com>
2022-08-22 14:34:03 +0100

    deploy contrat on infura

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.9",
+// };
+
+//new update from hardhat requires to import 'hardhat-toolbox'
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
